5d3652c5e1e325df710f347ad9c492fd128a868a
phhan2110020E1 <95125459+phhan2110020E1@users.noreply.github.com>
2022-02-24 17:13:59 +0700

    adding my files to my repository

---
+++ script.js
@@ -0,0 +1,15 @@
+function kiemtra(){
+    //email
+    var sEmail = document.getElementById("email").value;
+    if(sEmail.length == 0){
+        alert("The Email can not left empty!");
+        document.getElementById("email").focus();
+        return false;
+    }
+      var reEmail = /^\w+[@]\w+[.]\w{3}([.]\w{2})?$/;//alphabet from 6 to 12 characters
+      if(!reEmail.test(sEmail)){
+      alert("The Email is invalid!\n(The Email field must be matched email format xxxx@xxx.xxx or xxxx@xxx.xxx.xx)");
+      document.getElementById("email").focus();
+      return false;
+    }
+}
